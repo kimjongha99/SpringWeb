7df92b529ad1120b7f2286c5b2176f0a63500cd6
kimjongha99 <jjhsun99@gmail.com>
2023-08-03 16:50:31 +0900

    Update TodoDTO.java

--- springex/src/main/java/com/study/springex/dto/TodoDTO.java
+++ springex/src/main/java/com/study/springex/dto/TodoDTO.java
@@ -3,6 +3,10 @@ package com.study.springex.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @ToString
@@ -13,11 +17,13 @@ import java.time.LocalDate;
 public class TodoDTO {
     private Long tno;
 
+    @NotNull
     private String title;
 
+    @Future
     private LocalDate dueDate;
 
     private boolean finished;
-
+    @NotEmpty
     private String writer;
 }
