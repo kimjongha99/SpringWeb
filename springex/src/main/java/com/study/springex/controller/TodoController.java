5ffe1e83fe3420d5c6bbfc2197413b305da2da01
kimjongha99 <jjhsun99@gmail.com>
2023-08-03 16:50:27 +0900

    Update TodoController.java

--- springex/src/main/java/com/study/springex/controller/TodoController.java
+++ springex/src/main/java/com/study/springex/controller/TodoController.java
@@ -2,37 +2,97 @@ package com.study.springex.controller;
 
 
 import com.study.springex.dto.TodoDTO;
+import com.study.springex.service.TodoService;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+
 @Controller
 @Log4j2
 @RequestMapping("/todo")
+@AllArgsConstructor
 public class TodoController {
+    private final TodoService todoService;
+
 
     @RequestMapping("/list")
-    public void list() {
-        log.info("todo list");
+    public void list(Model model) {
+
+        log.info("todo list.......");
+
+        model.addAttribute("dtoList", todoService.getAll());
     }
 
-    //    @RequestMapping(value = "/register",method = RequestMethod.POST)
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public void registerGet() {
         log.info("GET      todo register-");
     }
 
     @PostMapping("/register")
-    public void  registerPost(TodoDTO todoDTO){
-        log.info("post reg~~~~~~~~~~~~~~~");
+    public String registerPost(@Valid TodoDTO todoDTO,
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes) {
+
+        log.info("POST todo register.......");
+
+        if (bindingResult.hasErrors()) {
+            log.info("has errors.......");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/todo/register";
+        }
+
         log.info(todoDTO);
+
+        todoService.register(todoDTO);
+
+        return "redirect:/todo/list";
+    }
+
+    @GetMapping({"/read", "/modify"})
+    public void read(Long tno, Model model) {
+
+        TodoDTO todoDTO = todoService.getOne(tno);
+        log.info(todoDTO);
+
+        model.addAttribute("dto", todoDTO);
 
     }
 
+    @PostMapping("/remove")
+    public String remove(Long   tno, RedirectAttributes redirectAttributes) {
 
+        log.info("----------------remove----------------");
+        log.info("tno: " + tno);
+        todoService.remove(tno);
+        return "redirect:/todo/list";
+    }
+
+
+    @PostMapping("/modify")
+    public String modify(@Valid TodoDTO todoDTO,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes){
+
+        if(bindingResult.hasErrors()) {
+            log.info("has errors.......");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors() );
+            redirectAttributes.addAttribute("tno", todoDTO.getTno() );
+            return "redirect:/todo/modify";
+        }
+
+        log.info(todoDTO);
+
+        todoService.modify(todoDTO);
+
+        return "redirect:/todo/list";
+    }
 }
