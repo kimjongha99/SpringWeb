bb8cb6e6093c01df43666e452e8052534f61d40b
kimjongha99 <jjhsun99@gmail.com>
2023-08-04 14:29:35 +0900

    Update TodoService.java

--- springex/src/main/java/com/study/springex/service/TodoService.java
+++ springex/src/main/java/com/study/springex/service/TodoService.java
@@ -1,6 +1,8 @@
 package com.study.springex.service;
 
 import com.study.springex.domain.TodoVO;
+import com.study.springex.dto.PageRequestDTO;
+import com.study.springex.dto.PageResponseDTO;
 import com.study.springex.dto.TodoDTO;
 
 import java.util.List;
@@ -8,8 +10,7 @@ import java.util.List;
 public interface TodoService {
     void register(TodoDTO todoDTO);
 
-    List<TodoDTO> getAll();
-
+    PageResponseDTO<TodoDTO> getList(PageRequestDTO pageRequestDTO);
 
     TodoDTO getOne(Long tno);
 
